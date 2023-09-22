1546a1c729bee661820944e6bbac2febbe857570
virajInCdac <viraj.in.cdac@gmail.com>
2023-09-22 11:45:39 +0530

    add comments to existing files and started assignemt for unix

--- viraj_cos/is_pallindrome.sh
+++ viraj_cos/is_pallindrome.sh
@@ -1,10 +1,12 @@
 read -p"enter string first - " s1
- 
+
+#lenght -1 to get actual word length in expression.
 l=`expr ${#s1} - 1`
 i=0
 echo $l
 while [ $i -lt $l ]
 do
+#here s1 is the input value and i indicates the starting digit of the inpput, wheras l indicates the last of the input. :1 gives character position 	
 if [ ${s1:$i:1} != ${s1:$l:1} ]
 then echo "not palibndrome"
 break
