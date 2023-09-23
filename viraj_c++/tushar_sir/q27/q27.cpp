30fa3db558052514234ae4887ba7a68c6af4a088
virajInCdac <viraj.in.cdac@gmail.com>
2023-09-23 19:14:06 +0530

    Done with Q27

--- viraj_c++/tushar_sir/q27/q27.cpp
+++ viraj_c++/tushar_sir/q27/q27.cpp
@@ -14,9 +14,11 @@ void accept(int matrix[3][3], int size){
             cin>>matrix[i][j];
         }
     }
+    cout<<endl;
 }
 
 void display(int matrix[3][3], int size){
+    cout<<"matrix is:\n";
     for (int i = 0; i < size; i++){
         for (int j = 0; j < size; j++){
             cout<<matrix[i][j]<<" ";
@@ -35,16 +37,35 @@ void transpose(int matrix[3][3], int size){
             matrix[i][j] = temp;
         }
     tracker_for_j++; //j should become 2, then 3 and so on.
-    cout<<"Value of J is "<<j<<endl;
+    //cout<<"Value of J is "<<j<<endl;
     }
 }
 
+void add (int matrix[3][3], int matrixo[3][3], int size)
+{
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            matrix[i][j]=matrix[i][j]+matrixo[i][j];
+        }
+    }
+}
 int main(){
-    int matrix[3][3];
-    accept(matrix, 3);
+    int matrix[3][3],matrix2[3][3];
+    // Accepting first Matrix
+        cout<<"Enter the elements for first matrix : ";
+        accept(matrix, 3);
     display(matrix, 3);
+    // Accepting Second Matrix
+        cout<<"Enter the elements for Second matrix 2 :\n";
+        accept(matrix2, 3);
+    display(matrix2, 3);
+    add(matrix2,matrix,3); //Will display at the end.
+    cout<<"transpose of first matrix 1";
     transpose(matrix, 3);
-    cout<<endl;
     display(matrix, 3);
+    cout<<"Additin of two matrix is:\n";
+    display(matrix2, 3);
+    
+    
     return 0;
 }
