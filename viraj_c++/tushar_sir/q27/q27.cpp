74d5773108b893c97e6fc2960f0d61338c20edb6
virajInCdac <viraj.in.cdac@gmail.com>
2023-09-23 18:18:23 +0530

    done with Q27

---
+++ viraj_c++/tushar_sir/q27/q27.cpp
@@ -0,0 +1,50 @@
+// 27. Write a program to accept and display 3 by 3 matrix. Write Accept() and Display() functions to
+// perform the tasks.
+
+// a. Find the transpose of a matrix and print the transpose using display() function.
+// b. Accept another matrix of same dimensions. Find the addition of two matrices and print the
+// resultant matrix.
+
+#include<iostream>
+using namespace std;
+
+void accept(int matrix[3][3], int size){
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            cin>>matrix[i][j];
+        }
+    }
+}
+
+void display(int matrix[3][3], int size){
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void transpose(int matrix[3][3], int size){
+    int i = 0, j =1;
+    int tracker_for_j = j;
+    for (i; i< size -1; i++){
+        for (j = tracker_for_j; j < size; j++){
+            int temp = matrix[j][i];
+            matrix[j][i] = matrix[i][j];
+            matrix[i][j] = temp;
+        }
+    tracker_for_j++; //j should become 2, then 3 and so on.
+    cout<<"Value of J is "<<j<<endl;
+    }
+}
+
+int main(){
+    int matrix[3][3];
+    accept(matrix, 3);
+    display(matrix, 3);
+    transpose(matrix, 3);
+    cout<<endl;
+    display(matrix, 3);
+    return 0;
+}
