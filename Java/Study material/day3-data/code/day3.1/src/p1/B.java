bb9f4cc70c9a701859672805ac3feac375665661
virajInCdac <145529869+virajInCdac@users.noreply.github.com>
2023-10-07 22:05:48 +0530

    Working on day 3 Java

---
+++ Java/Study material/day3-data/code/day3.1/src/p1/B.java
@@ -0,0 +1,8 @@
+//sub class in the same pkg
+package p1;
+class B extends A {
+ B() {
+    System.out.println("B's state "+i+" "+j+" "+k+" "+l);
+ }
+ 
+}
---
+++ Java/Study material/day3-data/code/day3.1/src/p2/D.java
@@ -0,0 +1,10 @@
+//sub class in a different pkg
+package p2;
+import p1.A;
+class D extends A {
+ D() {
+	 //super(); impl added by javac
+    System.out.println("D's state "+i+" "+j+" "+k+" "+l);
+ }
+ 
+}
