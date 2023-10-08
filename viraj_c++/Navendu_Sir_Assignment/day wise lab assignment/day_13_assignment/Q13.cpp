8aac551ed8eb1d6f33cdb10d9f304a5d57950817
virajInCdac <viraj.in.cdac@gmail.com>
2023-10-08 19:47:07 +0530

    Started with day 13 assignment c++

---
+++ viraj_c++/Navendu_Sir_Assignment/day wise lab assignment/day_13_assignment/Q13.cpp
@@ -0,0 +1,16 @@
+// Design inheritance hierarchy for following (for at least one) and implement it. Write a small program to demonstrate the use.
+// - Computer printers
+// - Bank accounts
+// - Different type of persons - Student, Employee, Artist.
+
+
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+
+    return 0;
+}
