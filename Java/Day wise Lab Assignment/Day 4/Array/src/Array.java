81ea7072a3eb09b408c2be8b5a3105a5d14046c3
virajInCdac <145529869+virajInCdac@users.noreply.github.com>
2023-10-09 05:54:09 -0700

    kuch nahi hua sirf day 4 ka data add kia

---
+++ Java/Day wise Lab Assignment/Day 4/Array/src/Array.java
@@ -0,0 +1,32 @@
+
+/*Objective -- Accept no of data samples(of type double) from User(using scanner)
+Create suitable array & display it using for-loop , to confirm default values.
+
+Accept data from User(scanner) & store it in the array.
+Display array data using  for loop.
+
+Display array data using for-each loop
+
+for-each loop(enhanced for loop)
+syntax
+eg : Display array data using for-each loop
+for(int i : data) //i=data[0] ,i=data[1],.....i=data[data.length-1]
+ sop(i);
+
+Important statement
+for-each loop Works on a copy of array element.
+
+for-each limitations
+1. Can only iterate from 1st elem to last elem , with step size +1
+2. Works on a copy of array elems
+(can't be used for modifying array elems).
+*/
+import java.util.Scanner;
+
+public class Array {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+
+}
